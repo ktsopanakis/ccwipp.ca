de72bb5cbe7c9ef16471fa4c2b684ccf6a84e428
ktsopanakis <k.tsopanakis@dotbydot.gr>
2022-06-11 19:28:50 +0300

    hugo issues

--- hugo/quickstart/themes/windytheme/assets/css/tailwind.config.js
+++ hugo/quickstart/themes/windytheme/assets/css/tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+    content: [
+        "../../layouts/**/*.{html,js}",
+        "../../layouts/*.{html,js}"
+    ],
+    purge: [
+            "../../layouts/**/*.{html,js}",
+            "../../layouts/*.{html,js}"
+    ],
     theme: {
       extend: {}
     },
