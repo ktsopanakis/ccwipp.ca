0b39cc4274b2f592a5f243395d24b7877cd09c7d
ktsopanakis <k.tsopanakis@dotbydot.gr>
2022-06-12 02:19:08 +0300

    more details

--- tailwind/_serve.js
+++ tailwind/_serve.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const nunjucks = require('nunjucks')
+const path = require('path')
 const app = express()
 const port = 3000
 const a = require('path').basename(__dirname);
@@ -10,10 +11,15 @@ nunjucks.configure('src', {
     autoescape: true,
     express: app
 });
+app.get('/output.css', function(req, res) {
+    res.sendFile(path.join(__dirname,"/temp/output.css"));
+});
+
 app.use(express.static('public'))
 app.get('/', function(req, res) {
     res.render('index.html');
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
