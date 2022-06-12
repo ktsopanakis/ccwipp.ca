a5924d0c666e836ad0bc35f6e4cc652bb3631b6c
ktsopanakis <k.tsopanakis@dotbydot.gr>
2022-06-12 12:48:19 +0300

    dynamic serving

--- tailwind/_serve.js
+++ tailwind/_serve.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const fs = require('fs-extra')
 const path = require('path')
 const app = express()
 const port = 3000
 const a = require('path').basename(__dirname);
 const data = require("./src/data/_data.json");
-
+const util = require('util')
+const readdir = util.promisify(fs.readdir)
 
 
 var livereload = require("livereload");
@@ -25,13 +27,49 @@ nunjucks.configure('src', {
 app.use(connectLiveReload());
 app.use(express.static('./.temp/public'))
 app.use(express.static('public'))
-app.get('/', function (req, res) {
 
-    res.render('pages/index.html', data['index']); 
-});
-app.get('/about', function (req, res) {
-    res.render('pages/about.html',data['about']); 
-});
+// for all the files in pages (and their subfolders) TODO: make this shit recursive
+readdir('src/pages', { withFileTypes: true })
+  .then(files => {
+    for (const dirent of files) {
+      if (!dirent.isDirectory()) {
+        const file = dirent.name
+        let route = '/'+path.parse(file).name;
+        if (route == '/index' ) {
+          route = '/'
+        }
+        const privatedata = data[path.parse(file).name];
+        const template = 'pages/'+file;
+        console.log('Serving page '+template+' on route '+route);
+        app.get(route, function (req, res) {
+          res.render(template, privatedata); 
+        });
+      } else {
+        readdir('src/pages/'+dirent.name, { withFileTypes: true })
+        .then(files => {
+          const dir=dirent.name
+          for (const dirent of files) {
+            if (!dirent.isDirectory()) {
+              const file = dirent.name
+              let route = '/'+dir+'/'+path.parse(file).name;
+              if (route == '/index' ) {
+                route = '/'
+              }
+              const privatedata = data[dir][path.parse(file).name];
+              const template = 'pages/'+dir+'/'+file;
+              console.log('Serving page '+template+' on route '+route);
+              app.get(route, function (req, res) {
+                res.render(template, privatedata); 
+              });
+            } else {
+              readdir('src/pages'+dirent.name, { withFileTypes: true })
+      
+            }
+          }
+        })
+      }
+    }
+  })
 
 
 app.listen(port, () => {
